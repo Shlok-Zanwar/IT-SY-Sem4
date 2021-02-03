ecdb1065fcb35cc151aa478638204709b157f5ec
Shlok-Zanwar <Shlok-Zanwar@users.noreply.github.com>
2021-02-03 13:13:21 +0530

    Update Assignment 1.py

--- Assignment 1.py
+++ Assignment 1.py
@@ -1,11 +1,15 @@
 myList = list(map(int, input("Enter the integers (space separated) : ").split()))
-myRevList = []
+myReverseList = []
+total = len(myList) - 1
 
-for i in range(len(myList)-1, -1, -1):
-    myRevList.append(myList[i])
+while total >= 0:
+    myReverseList.append(myList[total])
+    total -= 1
 
 sumList = []
 for i in range(len(myList)):
-    sumList.append(myList[i] + myRevList[i])
+    sumList.append(myList[i] + myReverseList[i])
 
-print(sumList)
+print("Input   = " + str(myList))
+print("Reverse = " + str(myReverseList))
+print("Sum     = " + str(sumList))
