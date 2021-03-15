c23802870594acd35284e456c59e1bda6de63709
Shlok-Zanwar <Shlok-Zanwar@users.noreply.github.com>
2021-03-15 22:32:32 +0530

    Commit

--- practice.py
+++ practice.py
@@ -21,39 +21,73 @@
 # print(checkSymm(matrix, noOfRows))
 
 
-noOfCases = int(input())
+# noOfCases = int(input())
 
-myList = []
+# myList = []
 
-for i in range(noOfCases):
-    arr = input().split()
-    arr[1] = int(arr[1])
+# for i in range(noOfCases):
+#     arr = input().split()
+#     arr[1] = int(arr[1])
 
-    notFound = True
-    for j in range(len(myList)):
-        if myList[j][0] == arr[0]:
-            myList[j][1] += arr[1]
-            myList[j][2] = i
-            notFound = False
-            break
+#     notFound = True
+#     for j in range(len(myList)):
+#         if myList[j][0] == arr[0]:
+#             myList[j][1] += arr[1]
+#             myList[j][2] = i
+#             notFound = False
+#             break
 
-    if notFound:
-        arr.append(i)
-        myList.append(arr)
+#     if notFound:
+#         arr.append(i)
+#         myList.append(arr)
 
-myList.sort(key=lambda x: x[1], reverse=True)
+# myList.sort(key=lambda x: x[1], reverse=True)
 
-myMax = myList[0][1]
-myName = myList[0][0]
-myIndex = myList[0][2]
+# myMax = myList[0][1]
+# myName = myList[0][0]
+# myIndex = myList[0][2]
 
-for i in range(1, len(myList)):
-    if myList[i][1] != myMax:
-        break
+# for i in range(1, len(myList)):
+#     if myList[i][1] != myMax:
+#         break
 
-    if myList[i][2] < myIndex:
-        myName = myList[i][0]
-        myIndex = myList[i][2]
+#     if myList[i][2] < myIndex:
+#         myName = myList[i][0]
+#         myIndex = myList[i][2]
 
-print(myName)
+# print(myName)
 # print(myList)
+
+# string = input("String : ")
+# toSearch = input("To Search : ");
+
+# index = 0
+
+# for i in range (0, len(string)):
+#     if string[i] == toSearch[index]:
+#         index += 1 
+#         if index == len(toSearch):
+#             print("Found")
+#             break
+#     else:
+#         index = 0
+
+# if i == len(string) - 1:
+#     print("Not Found")
+
+# import datetime
+# import calendar
+
+# current_time = "2021-02-15T11:58:23.588826+05:30"
+# # a = datetime.
+
+# year = current_time[0:4]
+# month = current_time[5:7]
+# day = current_time[8:10]
+
+# # myDate = datetime.date(int(year), int(month), int(day)).strftime("%b");
+
+# print(calendar.month_name[int(month)]);
+
+
+# import win32com.clie
